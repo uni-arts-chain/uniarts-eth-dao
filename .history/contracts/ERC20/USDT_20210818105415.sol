9826aadafcd6d6e64915480b52c4bfbb6ffd6a04
terry.tu <tuminfei1981@gmail.com>
2021-08-18 13:50:25 +0800

    NFT

--- .history/contracts/ERC20/USDT_20210818105415.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.8.0;
-
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-contract USDC_TOKEN is ERC20 {
-    constructor() ERC20("UsdCoin", "USDC") {
-        _mint(msg.sender, 1000000000000);
-    }
-}
