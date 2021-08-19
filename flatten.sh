3a1a4f60e78a12d75890261c5c8c6cff304516f4
terry.tu <tuminfei1981@gmail.com>
2021-08-19 11:02:58 +0800

    DutchAuction

--- flatten.sh
+++ flatten.sh
@@ -2,3 +2,4 @@ npx truffle-flattener contracts/ERC20/UINK.sol > flatten/UINK.sol
 npx truffle-flattener contracts/UinkTreasury.sol > flatten/UinkTreasury.sol
 npx truffle-flattener contracts/VoteMining.sol > flatten/VoteMining.sol
 npx truffle-flattener contracts/core/Marketplace.sol > flatten/Marketplace.sol
+npx truffle-flattener contracts/core/DutchAuction.sol > flatten/DutchAuction.sol 
