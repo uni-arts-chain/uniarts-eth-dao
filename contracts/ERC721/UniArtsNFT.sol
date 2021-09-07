0ed8b90c0e270dda25c480c2d579f2cdf5df862f
terry.tu <tuminfei1981@gmail.com>
2021-09-07 10:07:15 +0800

    add baseURI

--- contracts/ERC721/UniArtsNFT.sol
+++ contracts/ERC721/UniArtsNFT.sol
@@ -3,10 +3,13 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract UniArtsNFT is ERC721, AccessControl {
+contract UniArtsNFT is ERC721, ERC721Enumerable, AccessControl {
   // Create a new role identifier for the minter role
   bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+  string _baseURIValue;
 
   struct TokenInfo {
     string title;
@@ -17,10 +20,25 @@ contract UniArtsNFT is ERC721, AccessControl {
   /* Our tokeninfo are stored here */
   TokenInfo[] public token_infos;
 
-  constructor(address minter) public ERC721("UniArtsNFT", "UANFT") {
+  constructor(address minter, string memory baseURIValue_) ERC721("UniArtsNFT", "UANFT") {
     // Grant the minter role to a specified account
     _setupRole(MINTER_ROLE, minter);
     _setupRole(DEFAULT_ADMIN_ROLE, minter);
+    _baseURIValue = baseURIValue_;
+  }
+
+  function _baseURI() internal view override returns (string memory) {
+    return _baseURIValue;
+  }
+
+  function baseURI() public view returns (string memory) {
+    return _baseURI();
+  }
+
+  function setBaseURI(string memory newBase) public {
+    // Only admin can setBaseURI
+    require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "Caller is not a admin");
+    _baseURIValue = newBase;
   }
   
   function createNFT(address user_address, string calldata title, uint8 class_id, uint8 size) external {
@@ -47,13 +65,21 @@ contract UniArtsNFT is ERC721, AccessControl {
     );
   }
 
+  function _beforeTokenTransfer(
+      address from,
+      address to,
+      uint256 tokenId
+  ) internal virtual override(ERC721, ERC721Enumerable) {
+    super._beforeTokenTransfer(from, to, tokenId);
+  }
+
   /**
-  * override(ERC721, AccessControl) -> here you're specifying only two base classes ERC721, AccessControl
+  * override(ERC721, ERC721Enumerable, AccessControl) -> here you're specifying only two base classes ERC721, AccessControl
   * */
   function supportsInterface(bytes4 interfaceId)
       public
       view
-      override(ERC721, AccessControl)
+      override(ERC721, ERC721Enumerable, AccessControl)
       returns (bool)
   {
       return super.supportsInterface(interfaceId);
--- flatten.sh
+++ flatten.sh
@@ -2,6 +2,7 @@ npx truffle-flattener contracts/ERC20/UINK.sol > flatten/UINK.sol
 npx truffle-flattener contracts/UinkTreasury.sol > flatten/UinkTreasury.sol
 npx truffle-flattener contracts/VoteMining.sol > flatten/VoteMining.sol
 npx truffle-flattener contracts/Pin.sol > flatten/Pin.sol
+npx truffle-flattener contracts/ERC721/UniArtsNFT.sol > flatten/UniArtsNFT.sol
 npx truffle-flattener contracts/core/Marketplace.sol > flatten/Marketplace.sol
 npx truffle-flattener contracts/core/Auction.sol > flatten/Auction.sol 
 npx truffle-flattener contracts/core/DutchAuction.sol > flatten/DutchAuction.sol
