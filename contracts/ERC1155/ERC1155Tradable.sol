ae81a9cb07857b6e1c324ae1b9a245f4905bada3
terry.tu <tuminfei1981@gmail.com>
2021-12-22 17:02:01 +0800

    fix

--- contracts/ERC1155/ERC1155Tradable.sol
+++ contracts/ERC1155/ERC1155Tradable.sol
@@ -13,12 +13,6 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
-contract OwnableDelegateProxy { }
-
-contract ProxyRegistry {
-  mapping(address => OwnableDelegateProxy) public proxies;
-}
-
 /**
  * @dev Implementation of the basic standard multi-token.
  * See https://eips.ethereum.org/EIPS/eip-1155
@@ -47,8 +41,6 @@ contract ERC1155Tradable is Context, ERC165, IERC1155, IERC1155MetadataURI, Acce
 
     // Used as the URI for all token types by relying on ID substitution, e.g. https://token-cdn-domain/{id}.json
     string private _uri;
-
-    address public proxyRegistryAddress;
 
     // current TokenID
     uint256 private _currentTokenID = 0;
@@ -85,8 +77,7 @@ contract ERC1155Tradable is Context, ERC165, IERC1155, IERC1155MetadataURI, Acce
     constructor(
         string memory _name,
         string memory _symbol,
-        string memory _uri,
-        address _proxyRegistryAddress
+        string memory _uri
     ) {
         _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
         _setupRole(MINTER_ROLE, _msgSender());
@@ -95,7 +86,6 @@ contract ERC1155Tradable is Context, ERC165, IERC1155, IERC1155MetadataURI, Acce
 
         name = _name;
         symbol = _symbol;
-        proxyRegistryAddress = _proxyRegistryAddress;
         _setURI(_uri);
     }
 
@@ -212,11 +202,6 @@ contract ERC1155Tradable is Context, ERC165, IERC1155, IERC1155MetadataURI, Acce
      * @dev See {IERC1155-isApprovedForAll}.
      */
     function isApprovedForAll(address account, address operator) public view virtual override returns (bool) {
-        // Whitelist OpenSea proxy contract for easy trading.
-        // ProxyRegistry proxyRegistry = ProxyRegistry(proxyRegistryAddress);
-        // if (address(proxyRegistry.proxies(account)) == operator) {
-        //     return true;
-        // }
         return _operatorApprovals[account][operator];
     }
 
