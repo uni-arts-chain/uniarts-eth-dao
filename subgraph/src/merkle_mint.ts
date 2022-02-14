254eb0639bbd16f819a56dec57467c61ad070456
Terry Tu <tuminfei1981@gmail.com>
2022-02-14 22:38:31 +0800

    Update merkle_mint.ts

--- subgraph/src/merkle_mint.ts
+++ subgraph/src/merkle_mint.ts
@@ -6,10 +6,12 @@ export function handleClaimed(event: Claimed): void {
   let index = event.params.index
   let user_address = event.params.account
   let amount = event.params.amount
-  let id = event.transaction.hash.toHex() 
+  let id = event.transaction.hash.toHex()
+  let block_number = event.block.number
+  
   let entity = new TokenMint(index.toString())
-
   entity.user_address = user_address
   entity.amount = amount
+  entity.block_number = block_number
   entity.save()
 }
